512b89be2be91e340d6ed94fce094cf7864fe77f
adxell-arango-clickonplans <90437102+Adxell@users.noreply.github.com>
2022-10-05 14:49:14 -0500

    update name mongooConnection to mongoConnection fixing that type

--- database/db.ts
+++ database/db.ts
@@ -6,20 +6,20 @@ import mongoose, { mongo } from 'mongoose'
  * 2 = conneccting
  * 3 = disconnecting
 */
-const mongooConnection = {
+const mongoConnection = {
     isConnected: 0
 }
 
 
 export const connect = async() => {
-    if ( mongooConnection.isConnected ) {
+    if ( mongoConnection.isConnected ) {
         console.log('Already connected')
         return
     }
     if ( mongoose.connections.length > 0 ) {
-        mongooConnection.isConnected = mongoose.connections[0].readyState;
+        mongoConnection.isConnected = mongoose.connections[0].readyState;
 
-        if (mongooConnection.isConnected === 1 ) {
+        if (mongoConnection.isConnected === 1 ) {
             console.log('Using previous conection')
             return
         }
@@ -27,16 +27,16 @@ export const connect = async() => {
         await mongoose.disconnect()
     } 
     await mongoose.connect(process.env.MONGO_URL || '')
-    mongooConnection.isConnected = 1
+    mongoConnection.isConnected = 1
     console.log('Connecting to database: ', process.env.MONGO_URL)
 }
 
 export const disconnect = async() => {
     if ( process.env.NODE_ENV === 'development' ) return
-    if ( mongooConnection.isConnected === 0 ) return
+    if ( mongoConnection.isConnected === 0 ) return
 
     await mongoose.disconnect()
-    mongooConnection.isConnected = 0;
+    mongoConnection.isConnected = 0;
     console.log('Desconected database')
 }
 
